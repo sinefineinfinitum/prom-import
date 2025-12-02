de7ccede730baa759ab14130c7521b9f475cc8df
P Serg <sinefineagri@gmail.com>
2025-12-02 15:24:06 +0200

    init

---
+++ prom-import.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package           PromUaImport
+ * @version           0.0.1
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Prom Ua Import
+ * Plugin Uri:        https://wordpress.org/plugins/
+ * Description:       Import products from prom.ua to woo
+ * Version:           0.0.1
+ * Author:            P Serg
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       prom-import
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    die();
+}
---
+++ readme.txt
@@ -0,0 +1,6 @@
+=== REST API Products Importer for WooCommerce ===
+Contributors: pserg 
+Tags: woocommerce, products, prom.ua, importer, api
+Requires PHP: >=7.4
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
